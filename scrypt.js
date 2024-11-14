2f6f08441ad199ffcc175f8c336c0067d3ac5c7c
andreapanico12 <andreapanico12@gmail.com>
2024-11-14 14:41:38 +0100

    feat: add instrusctions and exercise approach to scrypt

---
+++ scrypt.js
@@ -0,0 +1,21 @@
+// ===============CONSEGNA=================
+// Scrivere un programma che stampi i numeri da 1 a 100.
+// Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// ================SVOLGIMENTO=============
+
+// 1. Identifico una singola variabile (let) chiamata "numeroStampato" senza però definirla.
+// 2. Creerò un ciclo "for" con varabile iniziale "i=1", con condizione "i<=100".
+// 3. All'interno di questo ciclo andrò a creare una serie di condizioni (if) per cui l'output di "numeroStampato" cambi in relazione alle specifiche condizioni date nella consegna.
+// 4. Comincerò a scrivere le mie condizioni specificando come prima l'essere divisibile per 3 e 5 poi a seguire la divisibilità per 3 ed in ultimo per 5. Questa sintassi è necessaria affinchè l'esito della divisibilità per 3 e 5 verrebbe altrimenti sovrascritto dalla prima condizione valida all'interno dell'if.
+// 5. Se nessuna delle condizioni date risulterà vera allora la variabile "numeroStampato" sarà uguale alla variabile "i".
+
+// ==============CODICE ESERCIZIO============
+
+// Dati
+
+let numeroStampato;
+
+// Logica
+
